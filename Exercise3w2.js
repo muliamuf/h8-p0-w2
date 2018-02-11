ee8b0571f9ad4440db8473f9fa014de6be221156
mac <mac@macs-iMac-4.local>
2018-02-11 08:02:53 +0700

    perbaikan Exercise 3 week 2

--- Exercise3w2.js
+++ Exercise3w2.js
@@ -1,17 +1,26 @@
-var nama = 'mufqi';
-var peran = 'Tabib';
-if (peran === '') {
+var nama = '';
+var peran = '';
+
+if (nama === '' && peran === ''){
+  console.log("Nama harus diisi!");
+}
+else if (nama !== '' && peran === '') {
+  console.log("Halo "+nama+", Pilih peranmu untuk memulai game!");
+}
+else{
+  if (peran === '') {
   console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
-} 
-else if (peran === 'Ksatria') {
+  }
+  else if (peran === 'Ksatria') {
   console.log('Selamat datang di Dunia Proxytia, ' + nama);
   console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjata mu!');
-} 
-else if (peran === 'Tabib') {
+  }
+  else if (peran === 'Tabib') {
   console.log('Selamat datang di Dunia Proxytia, ' + nama);
   console.log('Halo ' + peran + ' ' + nama + ',kamu akan membantu teman mu yang terluka.');
-} 
-else if (peran === 'Penyihir') {
+  }
+  else if (peran === 'Penyihir') {
   console.log('Selamat datang di Dunia Proxytia, ' + nama);
   console.log('Halo ' +peran +' ' +nama +',ciptakan keajaiban yang membantu kemenangan mu!');
+  }
 }
